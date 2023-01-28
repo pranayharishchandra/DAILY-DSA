7db1c02ab67090b3a9403e8d48564ad79d57da84
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-01-28 20:48:39 +0530

    Update 5.isGraphConnected.java

--- DSA/GRAPHS/EASY-MEDUIM/5.isGraphConnected.java
+++ DSA/GRAPHS/EASY-MEDUIM/5.isGraphConnected.java
@@ -1,66 +1,216 @@
 import java.io.*;
 import java.util.*;
 
+/* 
+ * JUST TRY WRITING -- CANPALACEVERTICLE or CANPLACEHORIZONTAL -- you may make mistake in writing those condions 
+ * REST ALL Func. are easy..  just go through them
+ */
+
 public class Main {
-    static class Edge {
+
+    public static class Edge {
+
         int src;
         int nbr;
         int wt;
 
-        Edge(int src, int nbr, int wt) {
-            this.src = src;
-            this.nbr = nbr;
-            this.wt = wt;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        char[][] arr = new char[10][10];
+        for (int i = 0; i < arr.length; i++) {
+            String str = scn.next();
+            arr[i] = str.toCharArray();
+        }
+        int n = scn.nextInt();
+        String[] words = new String[n];
+        for (int i = 0; i < words.length; i++) {
+            words[i] = scn.next();
+        }
+        scn.close();
+        solution(arr, words, 0);
+
+    }
+
+    public static void display(char[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr.length; col++) {
+                System.out.print(arr[row][col] + "");
+            }
+            System.out.println();
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    /*
+     * For placing each word we have to check the arr[][] from the start
+     * While traversing each cell of the arr[][] --> you have 2 options --> place
+     * vertically, place horiczontally
+     * But before checking if it (word) fits vertically of horizontally we must
+     * check if it's '-' or word.charAt(i)
+     */
 
-        int vtces = Integer.parseInt(br.readLine());
-        ArrayList<Edge>[] graph = new ArrayList[vtces];
-        for (int i = 0; i < vtces; i++) {
-            graph[i] = new ArrayList<>();
+    public static void solution(char[][] arr, String[] words, int vidx) {
+        if (vidx == words.length) {
+            display(arr);
+            return;
         }
 
-        int edges = Integer.parseInt(br.readLine());
-        for (int i = 0; i < edges; i++) {
-            String[] parts = br.readLine().split(" ");
-            int v1 = Integer.parseInt(parts[0]);
-            int v2 = Integer.parseInt(parts[1]);
-            int wt = Integer.parseInt(parts[2]);
-            // undirected graph
-            graph[v1].add(new Edge(v1, v2, wt));
-            graph[v2].add(new Edge(v2, v1, wt));
-        }
+        String word = words[vidx];
 
-        // write your code here
-        ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
-        boolean visited[] = new boolean[vtces];
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
 
-        for (int i = 0; i < vtces; i++) {
-            if (visited[i] == false) {
-                ArrayList<Integer> comp = new ArrayList<>();
-                getComps(graph, visited, i, comp);
-                comps.add(comp);
+                if (arr[row][col] == '-' || arr[row][col] == word.charAt(0)) {
+                    if (canPlaceHorizontally(arr, word, row, col)) {
+                        boolean visited[] = new boolean[word.length()];
+                        placeWordHorizontally(arr, word, row, col, visited);
+                        solution(arr, words, vidx + 1);
+                        unPlaceWordHorizontally(arr, word, row, col, visited);
+                    }
+
+                    else if (canPlaceVertically(arr, word, row, col)) {
+                        boolean visited[] = new boolean[word.length()];
+                        placeWordVertically(arr, word, row, col, visited);
+                        solution(arr, words, vidx + 1);
+                        unPlaceWordVertically(arr, word, row, col, visited);
+                    }
+                }
+
             }
         }
 
-        if (comps.size() == 1)
-            System.out.println("true");
-        else
-            System.out.println("false");
     }
 
-    // printing all the vertices connected with the source
-    static void getComps(ArrayList<Edge>[] graph, boolean[] visited, int src, ArrayList<Integer> comp) {
-        visited[src] = true;
-        comp.add(src);
+    public static boolean canPlaceHorizontally(char[][] arr, String word, int row, int col) {
+        // if there exists a cell then it must be '+' and if it don't exits then very
+        // good
+        if (col - 1 >= 0 && arr[row][col - 1] != '+')
+            return false;
 
-        for (Edge e : graph[src]) {
-            if (visited[e.nbr] == false) {
-                getComps(graph, visited, e.nbr, comp);
+        // if (col + word.length() != arr[0].length && col + word.length() != '+')
+
+        // CHECK THE LAST -- if cell exists then it must be '+'
+        // if (col + word.length() < arr[0].length && arr[row][col + word.length()] !=
+        // '+')
+        // return false;
+
+        int i;
+        for (i = 0; i < word.length(); i++) {
+            /* DON'T FORGET TO WRITE THE FOLLOWING CONDITION */
+            if (col + i >= arr[0].length)
+                return false;
+
+            // if (arr[row][i + col] != word.charAt(i) || arr[row][i + col] == '+')
+            // return false;
+
+            if ((arr[row][i + col] == word.charAt(i) || arr[row][i + col] == '-'))
+                continue;
+            else
+                return false;
+        }
+
+        /* YOU CAN ALSO DO THE FOLLOWING TO CHECK THE LAST */
+        if (i + col < arr[0].length && arr[row][i + col] != '+') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void placeWordHorizontally(char[][] arr, String word, int row, int col, boolean visited[]) {
+        for (int i = 0; i < word.length(); i++) {
+            if (arr[row][col + i] == '-') {
+                visited[i] = true;
+            }
+            arr[row][col + i] = word.charAt(i);
+        }
+    }
+
+    public static void unPlaceWordHorizontally(char[][] arr, String word, int row, int col, boolean visited[]) {
+        for (int i = 0; i < word.length(); i++) {
+            if (visited[i] == true) {
+                arr[row][col + i] = '-';
             }
         }
     }
+
+    public static boolean canPlaceVertically(char[][] arr, String word, int row, int col) {
+        // if there exists a cell then it must be '+' and if it don't exits then very
+        // good
+        if (row - 1 >= 0 && arr[row - 1][col] != '+')
+            return false;
+
+        // if (row + word.length() != arr.length && row + word.length() != '+') // this
+        // is wrong
+
+        // CHECKING THE LAST
+        // if (row + word.length() < arr.length && arr[row + word.length()][col] != '+')
+        // // this is wrong
+        // return false;
+
+        int i;
+        for (i = 0; i < word.length(); i++) {
+
+            /* DON'T FORGET TO WRITE THE FOLLOWING CONDITION */
+            if (row + i >= arr.length)
+                return false;
+
+            // if (arr[row + i][col] != word.charAt(i) || arr[row + i][col] == '+')
+            // return false;
+
+            /*
+             * ABOVE CONDITION IS WRONG -- BELOW IS CORRECT
+             * THE ABOVE COND. WILL ALWAYS BE TRUE -- AND 'FALSE' WILL ALWAYS BE RETURNED
+             * SO YOUR canPlaceVertically is always false
+             * 
+             * NOTE : WHEN USING (OR)(||) MAKE SURE YOU WRITE
+             * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+             * --> FAVOURABLE CONDITION <-- (favourable condition 1 || falvourable cond. 2
+             * || fav. cond. 3 || .... so on ... )
+             * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+             * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+             * 
+             * NOTE : WHEN USING (AND)(&&) MAKE SURE YOU WRITE
+             * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+             * --> UNFAVOURABLE CONDITION <-- (unfavourable condition 1 && unfalvourable
+             * cond. 2 && unfav. cond. 3 && .... so on ... )
+             * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+             * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+             * 
+             */
+            if ((arr[i + row][col] == word.charAt(i) || arr[i + row][col] == '-'))
+                continue;
+            else
+                return false;
+        }
+
+        /*
+         * YOU CAN ALSO THE THE FOLLOWING -- IT'S CORRECT
+         * VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+         */
+        if (i + row < arr[0].length && arr[i + row][col] != '+') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void placeWordVertically(char[][] arr, String word, int row, int col, boolean visited[]) {
+        for (int i = 0; i < word.length(); i++) {
+            if (arr[row + i][col] == '-') {
+                visited[i] = true;
+            }
+            arr[row + i][col] = word.charAt(i);
+        }
+    }
+
+    public static void unPlaceWordVertically(char[][] arr, String word, int row, int col, boolean visited[]) {
+        for (int i = 0; i < word.length(); i++) {
+            if (visited[i] == true) {
+                arr[row + i][col] = '-';
+            }
+        }
+    }
+
 }
