5b69ccff138972bcd2f63b4e61876adcfaaf03d8
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-02-08 00:02:42 +0530

    Update 18.bipartiteGraphDFS.cpp

--- DSA/GRAPHS/STRIVER/18.bipartiteGraphDFS.cpp
+++ DSA/GRAPHS/STRIVER/18.bipartiteGraphDFS.cpp
@@ -1,32 +1,116 @@
-private: 
-    bool dfs(int node, int col, int color[], vector<int> adj[]) {
-        color[node] = col; 
-        
-        // traverse adjacent nodes
-        for(auto it : adj[node]) {
-            // if uncoloured
-            if(color[it] == -1) {
-                if(dfs(it, !col, color, adj) == false) return false; 
+class Solutoion1
+{
+private:
+    // DFS SOL1           WITH using currCle paremeter-------VVVVVVV
+    bool check(int src, int V, vector<int> adj[], int color[], int currClr)
+    {
+        // mark visited and work
+        color[src] = currClr;
+
+        // calling the unvisited nbr
+        // if visited then check if have same color then not bipertite graph
+        // if different color the ignore it
+        for (auto nbr : adj[src])
+        {
+            if (color[nbr] == -1)
+            {
+                // call the unvisited nbr
+                if (check(nbr, V, adj, color, !currClr) == false) return false;
+                
             }
-            // if previously coloured and have the same colour
-            else if(color[it] == col) {
-                return false; 
+            // if already visited the check -- the color should not be same
+            else if (color[src] == color[nbr]) return false;
+
+            
+        }
+
+        return true; // if color[src] == color[nbr] was never found -- bipertite graph
+    }
+
+public:
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        int color[V];
+
+        for (int i = 0; i < V; i++) color[i] = -1;
+
+        for (int i = 0; i < V; i++)
+        {
+            int src = i;
+            if (color[src] == -1)
+            {
+                if (check(src, V, adj, color, 0) == false) return false;
             }
         }
-        
-        return true; 
+
+        return true; // all the components are bipertite
     }
+}
+
+
+
+
+
+
+
+
+
+class Solution2
+{
+private:
+    // DFS SOL2              whitout using currCle paremeter-------VVVVVVV
+    bool check(int src, int V, vector<int> adj[], int color[], int currClr)
+    {
+        // mark visited and work
+        color[src] = currClr;
+
+        // calling the unvisited nbr
+        // if visited then check if have same color then not bipertite graph
+        // if different color the ignore it
+        for (auto nbr : adj[src])
+        {
+            if (color[src] == -1)
+            {
+                // call the unvisited nbr
+                color[nbr] = !color[src];
+                if (check(nbr, V, adj, color, !currClr) == false)
+                {
+                    return false;
+                }
+            }
+            else // if already visited the check -- the color should not be same
+            {
+                if (color[src] == color[nbr])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true; // if color[src] == color[nbr] was never found -- bipertite graph
+    }
+
 public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    int color[V];
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    // for connected components
-	    for(int i = 0;i<V;i++) {
-	        if(color[i] == -1) {
-	            if(dfs(i, 0, color, adj) == false) 
-	                return false; 
-	        }
-	    }
-	    return true; 
-	}
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        int color[V] = {-1};
+
+        for (int i = 0; i < V; i++) color[i] = -1;
+        
+
+        for (int i = 0; i < V; i++)
+        {
+            int src = i;
+            if (color[src] == -1)
+            {
+                color[src] = 0;
+                if (check(src, V, adj, color, 0) == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true; // all the components are bipertite
+    }
+};
