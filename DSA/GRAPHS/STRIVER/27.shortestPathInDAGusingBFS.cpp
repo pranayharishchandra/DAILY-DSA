598e583f74abe140cf5d1ae2e319b2a686eebad6
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-02-13 21:21:57 +0530

    BFS SOLUTION -- RECOMMENDED
    it's better in my opinion than using that DFS method
    since it can only be using in DAG 
    where as this BFS method can be used anytime anywhere

---
+++ DSA/GRAPHS/STRIVER/27.shortestPathInDAGusingBFS.cpp
@@ -0,0 +1,69 @@
+class Solution
+{
+// MODIFICATION OF 28th QUESION -- Shortest path in Undirected Graph having unit distance
+public:
+    // vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int Qsrc)
+    vector<int> shortestPath(int N,int M, vector<vector<int>>& edges)
+    {
+        // code here
+        int Qsrc = 0;
+
+        // CREATING ADJ LIST ==> SRC - {{nbr, wt}, {nbr2, wt2},....}
+        vector<pair<int, int>> adj[N];
+
+        for (auto edge : edges)
+        {
+            int src = edge[0];
+            int nbr = edge[1];
+            int wt  = edge[2];
+
+            adj[src].push_back(make_pair(nbr, wt));
+            // adj[nbr].push_back(make_pair(src, wt)); // <-- DAG (directed)
+        }
+
+        int dis[N];
+
+        for (int i = 0; i < N; i++)
+            dis[i] = INT_MAX;
+
+        queue<int> q;
+
+        q.push(Qsrc);
+        dis[Qsrc] = 0;
+
+        while (q.size() > 0)
+        {
+            int src = q.front();
+            q.pop();
+
+            for (auto edge : adj[src])
+            {
+                int nbr = edge.first;
+                int wt = edge.second;
+
+                if (dis[nbr] > dis[src] + wt)
+                {
+                    dis[nbr] = dis[src] + wt;
+                    q.push(nbr);
+                }
+            }
+        }
+
+        vector<int> dist(N, -1);
+        for (int i = 0; i < N; i++)
+        {
+            if (dis[i] != INT_MAX)
+            {
+                dist[i] = dis[i];
+            }
+        }
+
+        return dist;
+    }
+};
+
+/*
+ * LINE 47 - pushing only when we get a shorter path -- which is not possible if we go front and come back (wt x 2) -- so visited not required
+ * there can't be updation when going to nbr and again coming back
+ * this is new type of visited -- just learn this method in these types of questions
+ */
