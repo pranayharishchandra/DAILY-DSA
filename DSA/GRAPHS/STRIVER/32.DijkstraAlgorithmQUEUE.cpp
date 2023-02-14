ff2693992170426f6cb99e0bc946f814073dc1b4
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-02-14 17:50:00 +0530

    Dijkstra Algorithm -- QUEUE

--- DSA/GRAPHS/STRIVER/32.DijkstraAlgorithmQUEUE.cpp
+++ DSA/GRAPHS/STRIVER/32.DijkstraAlgorithmQUEUE.cpp
@@ -43,7 +43,9 @@ public:
                 int nbr = edge[0];
                 int wt  = edge[1];
 
-                if (dis[nbr] > dis[src] + wt)
+                if (dis[nbr] > dis[src] + wt) // UPDATE DISTANCE WHEN YOU GET A SHORTER DISTANCE TO A NODE 
+			                      // AND THEN PUSH IS INTO QUEUE SO THIS NBR CAN ACT AS SRC FOR IT'S NBRS 
+			                      // and that's how u will find distance for unexplored nodes from the src
                 {
                     dis[nbr] = dis[src] + wt;
                     q.push(nbr);
@@ -74,4 +76,12 @@ public:
  * Original parameters -- of this question
  * // from the source vertex S.
  * vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+ */
+
+/* queue is the slowest better use priority_queue
+ * negative weights can't be handled by the Dijkstra Algorithm 
+ * you can do a dry run -- of 2 nodes and a negative edge between them 
+ * you will notice that the negaive will keep increasing -1 + (-1) = -2 
+ * and -2 is better tham -1 in since Dijkstra Algorithm updates when you get shorter a distance to a node -- LINE - 46 
+ * so it will become infinte as negative will keep on increasing according to this condition.
  */
