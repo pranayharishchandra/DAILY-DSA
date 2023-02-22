87afa495c2f66d371b0833e1eb60ce19db2e8d66
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-02-22 20:54:17 +0530

    PRIMS ALGO - CONSTURCT MST & MIN SUM CALC

--- DSA/GRAPHS/STRIVER/45.primsAlgo.cpp
+++ DSA/GRAPHS/STRIVER/45.primsAlgo.cpp
@@ -1,65 +1,64 @@
 class Solution
 {
+        // overall time complexity --> O(E x log E) + O(E x log E) = O(E x log E)
 public:
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // since you have to find min wt -- choose pq -- for lesser no of iterations
         // make a priority queue -- {wt,{node,parent}} and visited
-        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        vector<int> vis(V, 0);
+        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
+        vector<int> vis(V,0);
 
-        // push src and mark it visited
-        // let 0 be the src since no src is given
         pq.push({0,{0,-1}});
-        // vis[0] = 1; // don't mark visited here otherwise 0 will be continued and it's nbrs will not be called
+        // vis[0] = 1; // don't do it here
 
-        bool flag = false;
-        int sum = 0;
+        bool flag = false;         // using it to ignore the first pair into MST i.e. (parent, src) (0, -1)
+        int   sum = 0;
+        vector<pair<int,int>> MST; // {{parent,src}}
         
-        // if the question asked you to construct one of the min spanning tree
-        vector<pair<int,int>> constructSpanningTree;
-
-        // in worst case our pq will have all edges -- so our while loop is gonna run E times
-        while (pq.size() > 0)
+        // time complexity of this loop --> E x log E
+        while (pq.size() > 0) // O(E) // E edges will be inserted into it in worst case
         {
-            auto it = pq.top();
+            auto it = pq.top(); // O(log E)
             pq.pop();
 
-            int     wt = it.first;
-            int    src = it.second.first;
-            int parent = it.second.second;
+            int wt     = it.first;
+            int src    = it.second.first;
+            int parent = it.second.first;
 
-            if (vis[src] == 1) continue; // this line is actually not required since alread we mark visited in for loop
-            else vis[src] = 1;
+            if (vis[src] == 1) continue;     // if already visited
+            else               vis[src] = 1; // mark it as visited
 
             sum += wt;
-            if (flag == true)   constructSpanningTree.push_back({paren, src});
-            else if (flag == false) flag = true;
-            
-            // cout << "src  = " << src << endl;
+            if (flag == true) {
+                flag = true;
+                // continue;                  // since we have to add it's nbrs as well
+            }
+            else {
+                MST.push_back({parent,src});
+            }
 
-            for (auto ed : adj[src])
+            // time complexity of this loop --> E x log E
+            for (auto ed : adj[src])             // max E edges can be possibe of a node
             {
                 int nbr  = ed[0];
                 int edwt = ed[1];
 
-                // add into pq ~if you get a better~ all the edges to the nbr which is unvisited 
-                if (vis[nbr] == 1) continue;
-                else if (vis[nbr] == 0)
-                {
-                    pq.push({edwt, {nbr, src}});
+                if (vis[nbr] == 1) continue;     // if nbr was visited.. means we have already got answer for it so don't add it again in pq
+                else {
+                    pq.push({edwt, {nbr, src}}); // O(log E) // {edwt, {src, parent}} 
                 }
             }
         }
-        
-        // printing out min spanning tree.. code is working fine for it as well
-        for (auto it :constructSpanningTree ) cout << it.first << " " << it.second << endl;
-        
-        return sum; 
+
+        // for (auto it : MST) cout << it.first << " " << it.second << endl;
+        return sum;
     }
 };
+
+
 /*
- * NOTE 1:
+ * NOTE 1: 1ST OCCURANCE OF A NODE TOP OF PQ ONLY HAS SIGNIFICANCE.
  * you may push a node (let's say node1) more than once but whenever the node1 will be on the top of the pq
  * then it will always be the node1 with the min distance/weight on the top of pq
  * after it all the node1 with wt2,or may be wt3 and so on will be always more than w1
@@ -69,4 +68,17 @@ public:
  * here, already visited previously ... IGNORE
  
  * and to save one of such useless iteration(s) we use set and erase them in DIJKSTRA'S ALGO.
+ */
+
+/*
+ * NOTE 2: CAN'T USE QUEUE
+ * UNLIKE DIJKSTRA'S ALGO WE CAN'T SOLVE THIS PROBLEM ALSO USING QUEUE
+ * because we only want the 1st and smallest wt having edge from the nbr.
+ * then you will get sum more than or equal to the min sum... since your program is acutally not considering the smallest wt edge... so this point was obvious.
+ */
+
+/*
+ * NOTE 3: 
+ * use parent when you are asked to construct the MST -- in this question they haven't asked to us to create it
+ * but to write code of both construct and sum in once prog. I have used it.
  */
