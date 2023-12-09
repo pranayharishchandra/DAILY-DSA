3f3bbd1b87c7cf59614485dd5b378212514c7546
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-12-09 20:38:22 +0530

    Update DP 10. Minimum Path Sum in Grid.cpp

--- DSA/DYNAMIC PROGRAMMING/DP 10. Minimum Path Sum in Grid.cpp
+++ DSA/DYNAMIC PROGRAMMING/DP 10. Minimum Path Sum in Grid.cpp
@@ -1,6 +1,24 @@
 /*============================================================================================================
+    APPROACH: SOLUTION - 1, 2
+    we are calling from the last cell of the matrix --> n-1, m-1
+    and we are going to                             -->   0, 0
+
+    0,0 we have a base case
+
+    then again we are coming back to n-1, m-1
+
+    since here we are talking about the min sum: 1e9
+    if it was asked max sum: -1e9
+    if it was asked ways/ number of ways: 0
+
+============================================================================================================*/
+
+/*============================================================================================================
     SOLUTION - 1 : MOMOIZATION - dumb function call
     note: don't use INT_MAX, use 1e9
+
+    TC: mxn
+    SC: mxn + (m-1 + n-1) i.e. path le
 
     reason : The memoization Approach please don't take INT_MAX in the base condition for 
         (i<0 || j<0) as when we will reach the base condition it will be added to the value 
@@ -40,21 +58,13 @@ public:
 
 
 /*============================================================================================================
-    SOLUTION - 2 : MOMOIZATION - smart function call
-    note: don't use INT_MAX, use 1e9
-
-    reason : The memoization Approach please don't take INT_MAX in the base condition for 
-        (i<0 || j<0) as when we will reach the base condition it will be added to the value 
-        of previous state which is grid[i][j] , and it will lead to integer overflow ,
-        so just please use a bigger number like 1e9 as striver has done. In case anyone is wondering
+    SOLUTION - 2 : MOMOIZATION - smart function call 
+    TC: mxn
+    SC: mxn + (m-1 + n-1) i.e. path len
 ============================================================================================================*/
 class Solution {
 public:
     int minSumPathUtil(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp) {
-        if (i < 0 || j < 0) {
-            // If out of bounds, return a large value
-            return 1e9;
-        }
 
         if (i == 0 && j == 0) {
             return matrix[0][0];
@@ -64,10 +74,13 @@ public:
             return dp[i][j];
         }
 
-        int up = matrix[i][j] + minSumPathUtil(i - 1, j, matrix, dp);
-        int left = matrix[i][j] + minSumPathUtil(i, j - 1, matrix, dp);
+        int up = 1e9;
+        if (i-1 >= 0) up = minSumPathUtil(i - 1, j, matrix, dp);
+        
+        int left = 1e9;
+        if (j-1 >= 0) left = minSumPathUtil(i, j - 1, matrix, dp);
 
-        return dp[i][j] = min(up, left);
+        return dp[i][j] = matrix[i][j] + min(up, left);
     }
 
     int minPathSum(vector<vector<int>> &matrix) {
@@ -77,3 +90,11 @@ public:
         return minSumPathUtil(n - 1, m - 1, matrix, dp);
     }
 };
+
+/*============================================================================================================
+    SOLUTION - 3 : TABULATION - (TABULIZING SOLUTION 2)
+    
+============================================================================================================*/
+
+
+
