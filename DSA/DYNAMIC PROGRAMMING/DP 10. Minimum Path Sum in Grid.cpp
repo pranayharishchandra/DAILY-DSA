cf94897ff740eeea93c931ea2d3ba85d376b9993
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-12-09 20:09:21 +0530

    Update DP 10. Minimum Path Sum in Grid.cpp

--- DSA/DYNAMIC PROGRAMMING/DP 10. Minimum Path Sum in Grid.cpp
+++ DSA/DYNAMIC PROGRAMMING/DP 10. Minimum Path Sum in Grid.cpp
@@ -1,5 +1,5 @@
 /*============================================================================================================
-    SOLUTION - 1 : MOMOIZATION
+    SOLUTION - 1 : MOMOIZATION - dumb function call
     note: don't use INT_MAX, use 1e9
 
     reason : The memoization Approach please don't take INT_MAX in the base condition for 
@@ -35,4 +35,45 @@ public:
         return minSumPathUtil(n - 1, m - 1, matrix, dp); 
     }
 
+};
+
+
+
+/*============================================================================================================
+    SOLUTION - 2 : MOMOIZATION - smart function call
+    note: don't use INT_MAX, use 1e9
+
+    reason : The memoization Approach please don't take INT_MAX in the base condition for 
+        (i<0 || j<0) as when we will reach the base condition it will be added to the value 
+        of previous state which is grid[i][j] , and it will lead to integer overflow ,
+        so just please use a bigger number like 1e9 as striver has done. In case anyone is wondering
+============================================================================================================*/
+class Solution {
+public:
+    int minSumPathUtil(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0) {
+            // If out of bounds, return a large value
+            return 1e9;
+        }
+
+        if (i == 0 && j == 0) {
+            return matrix[0][0];
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int up = matrix[i][j] + minSumPathUtil(i - 1, j, matrix, dp);
+        int left = matrix[i][j] + minSumPathUtil(i, j - 1, matrix, dp);
+
+        return dp[i][j] = min(up, left);
+    }
+
+    int minPathSum(vector<vector<int>> &matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return minSumPathUtil(n - 1, m - 1, matrix, dp);
+    }
 };
