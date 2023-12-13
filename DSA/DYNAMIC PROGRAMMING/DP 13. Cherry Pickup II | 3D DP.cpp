86f739c868e6d867c73d615b9e6b41e7361120ca
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2023-12-13 22:38:02 +0530

    Update DP 13. Cherry Pickup II | 3D DP.cpp

--- DSA/DYNAMIC PROGRAMMING/DP 13. Cherry Pickup II | 3D DP.cpp
+++ DSA/DYNAMIC PROGRAMMING/DP 13. Cherry Pickup II | 3D DP.cpp
@@ -1,1 +1,17 @@
+/*============================================================================================================
+  CATEGORY: 3D DP
+            -- alice and bob, both travelling on the same grid
+            -- they both are comming down by 1 step every time so i+1 everytime
+            -- so "i" will be common in both
+            -- for columns, j1, j2 -- for alice and bob
+============================================================================================================*/
+
+
+
+
+
+
+
+
+
 
