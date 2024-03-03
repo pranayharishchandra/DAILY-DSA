8eb05bd8dc3ff76117495b80ffbc36f0161ad2c6
pranay <pranayharishchandra@gmail.com>
2024-03-03 20:09:00 +0530

    03 Frog K Jump

--- STRIVER 450/DYNAMIC PROGRAMMING/1D DP/02 Frog Jump.cpp
+++ STRIVER 450/DYNAMIC PROGRAMMING/1D DP/02 Frog Jump.cpp
@@ -13,9 +13,11 @@ Reason: We are using a recursion stack space(O(N)) and an array (again O(N)). Th
 int solve(int ind, vector<int>& height, vector<int>& dp){
     if(ind==0) return 0;
     if(dp[ind]!=-1) return dp[ind];
-    int jumpTwo = INT_MAX;
+
     int jumpOne= solve(ind-1, height,dp)+ abs(height[ind]-height[ind-1]);
-    if(ind>1)
+
+    int jumpTwo = INT_MAX;
+    if(ind>=2)
         jumpTwo = solve(ind-2, height,dp)+ abs(height[ind]-height[ind-2]);
     
     return dp[ind]=min(jumpOne, jumpTwo);
@@ -38,11 +40,14 @@ Reason: We are using an external array of size ‘n+1’.
 int f(vector<int> height) {
   int n=height.size();
   vector<int> dp(n,-1);
+
+  // energy required to go from the stair on which we are to which we are is 0
   dp[0]=0;
+
   for(int ind=1;ind<n;ind++){
       int jumpTwo = INT_MAX;
         int jumpOne= dp[ind-1] + abs(height[ind]-height[ind-1]);
-        if(ind>1)
+        if(ind>=2)
             jumpTwo = dp[ind-2] + abs(height[ind]-height[ind-2]);
     
         dp[ind]=min(jumpOne, jumpTwo);
@@ -64,8 +69,9 @@ int f(vector<int> height) {
   int prev2=0;
   for(int i=1;i<n;i++){
       
-      int jumpTwo = INT_MAX;
       int jumpOne= prev + abs(height[i]-height[i-1]);
+
+      int jumpTwo = INT_MAX;
       if(i>1)
         jumpTwo = prev2 + abs(height[i]-height[i-2]);
     
---
+++ STRIVER 450/DYNAMIC PROGRAMMING/1D DP/03 Frog K Jump.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+using namespace std;
+
+/** Prev question
+int solve(int ind, vector<int>& height, vector<int>& dp){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+
+    int jumpOne= solve(ind-1, height,dp)+ abs(height[ind]-height[ind-1]);
+
+    int jumpTwo = INT_MAX;
+    if(ind>=2)
+        jumpTwo = solve(ind-2, height,dp)+ abs(height[ind]-height[ind-2]);
+    
+    return dp[ind]=min(jumpOne, jumpTwo);
+}
+*/
+int solveUtil(int ind, vector<int>& height, vector<int>& dp, int k) {
+
+    if (ind == 0) return 0; // base case
+
+    if (dp[ind] != -1) return dp[ind];
+
+    int subAns = INT_MAX;
+
+    // frog will do jump of min size 1 and max size k
+    for (int j = 1; j <= k; j++) {
+        if (ind - j >= 0) {
+            int jump = solveUtil(ind - j, height, dp, k) + abs(height[ind] - height[ind - j]);
+            subAns = min(jump, subAns);
+        }
+    }
+    return dp[ind] = subAns;
+}
+
+int solve(int n, vector<int>& height, int k) {
+    vector<int> dp(n, -1);
+    return solveUtil(n - 1, height, dp, k);
+}
+
+/**============= Tabulation =================
+Time Complexity: O(N*K)
+Reason: We are running two nested loops, where outer loops run from 1 to n-1 and the inner loop runs from 1 to K
+
+Space Complexity: O(N)
+Reason: We are using an external array of size ‘n’.
+*/
+
+int solveUtil(int n, vector<int>& height, vector<int>& dp, int k) {
+    dp[0] = 0;
+    for (int i = 1; i < n; i++) {
+      
+        int subAns = INT_MAX;
+        for (int j = 1; j <= k; j++) {
+            if (i - j >= 0) {
+                int jump = dp[i - j] + abs(height[i] - height[i - j]);
+                subAns = min(jump, subAns);
+            }
+        }
+        dp[i] = subAns;
+
+    }
+    return dp[n - 1];
+}
+
+int solve(int n, vector<int>& height, int k) {
+    vector<int> dp(n, -1);
+    return solveUtil(n, height, dp, k);
+}
+
+
